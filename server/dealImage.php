77190b2e0456813742ef8297846d73c09d19fdd5
xym <727347070@qq.com>
2020-02-07 10:08:53 +0800

    图片上传 数据组织

--- server/dealImage.php
+++ server/dealImage.php
@@ -4,3 +4,14 @@
  * Date: 2020/2/6
  * Time: 下午2:11
  */
+
+echo '<pre>';
+print_r($_POST);
+
+$data=$_POST;
+
+foreach ($data['img'] as $k=>$v){
+    $arr[$k]['goods_id']=1;//管理的属性id  默认1
+    $arr[$k]['img']=$v;
+}
+print_r($arr);
