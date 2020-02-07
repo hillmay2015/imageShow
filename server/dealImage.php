9a2c4e21754118b05308ecfd1d0ca8e6c5e88f13
xym <727347070@qq.com>
2020-02-07 11:14:57 +0800

    将图片存到数据库

--- server/dealImage.php
+++ server/dealImage.php
@@ -5,13 +5,30 @@
  * Time: 下午2:11
  */
 
-echo '<pre>';
-print_r($_POST);
+// 假定数据库用户名：root，密码：root，数据库：zoo
+$con=mysqli_connect("localhost:8889","root","root","zoo");
+if (mysqli_connect_errno($con))
+{
+    echo "连接 MySQL 失败: " . mysqli_connect_error();
+}
+
 
 $data=$_POST;
 
 foreach ($data['img'] as $k=>$v){
     $arr[$k]['goods_id']=1;//管理的属性id  默认1
     $arr[$k]['img']=$v;
+    $query="INSERT INTO picture (good_id,img) VALUES ('1','$v')";
+    echo $query."<br>";
+    $result=mysqli_query($con,$query);
+    // 输出自动生成的 ID
+    echo "插入成功,新 id 为: " . mysqli_insert_id($con);
 }
-print_r($arr);
+
+
+mysqli_close($con);
+
+?>
+
+
+
